7e33a92b87ea7a3d881bbc72812143221427619b
Nelson Gomez <ng2573@columbia.edu>
2018-03-20 03:36:30 -0400

    Update test script

--- test.sh
+++ test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 TESTS_DIR="./tests"
+RETVAL=0
 
 for file in $TESTS_DIR/*.ic
 do
-	eval "./automatic.native < ${file} > tmp 2> /dev/null"
+	eval "./automatic.native -a < ${file} > tmp 2> /dev/null"
 	filename=$(eval "basename ${file%.*}")
 	
 	diff=$(eval "diff tmp ${TESTS_DIR}/${filename}.out")
@@ -16,7 +17,10 @@ do
 		printf '%s\n' "OK"
 	else
 		printf '%s\n' "NOPE"
+		RETVAL=1
 	fi
 
 	rm tmp
-done	
+done
+
+exit $RETVAL
